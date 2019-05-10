7b81948e0a31cde3cbf0b44a960030bb1a66e9ec
麻智敏 <ZhiMinMa@sf-express.com>
2019-05-10 19:51:28 +0800

    调整端口

---
+++ src/main/java/com/ch/cloud/gateway/conf/SwaggerConfiguration.java
@@ -0,0 +1,7 @@
+package com.ch.cloud.gateway.conf;
+
+/**
+ * @author 01370603
+ */
+public class SwaggerConfiguration {
+}
---
+++ src/main/java/com/ch/cloud/gateway/service/SsoService.java
@@ -0,0 +1,7 @@
+package com.ch.cloud.gateway.service;
+
+/**
+ * @author 01370603
+ */
+public interface SsoService {
+}
