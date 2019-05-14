608a8df2bac4fc233d0851e6f28b0b2b2da7f51d
仙_梦如尘 <zhimin711@sina.com>
2019-05-14 08:57:25 +0800

    添加动态路由

--- src/main/java/com/ch/cloud/gateway/fallback/ServiceFallbackProvider.java
+++ src/main/java/com/ch/cloud/gateway/fallback/ServiceFallbackProvider.java
@@ -1,6 +1,6 @@
 package com.ch.cloud.gateway.fallback;
 
-import com.ch.e.CoreError;
+import com.ch.e.PubError;
 import com.ch.result.Result;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
@@ -56,7 +56,7 @@ public class ServiceFallbackProvider implements FallbackProvider {
             public InputStream getBody() throws IOException {
                 //响应体
                 ObjectMapper objectMapper = new ObjectMapper();
-                String content = objectMapper.writeValueAsString(new Result<>(CoreError.CONNECT, route + "微服务不可用，请稍后再试..."));
+                String content = objectMapper.writeValueAsString(Result.error(PubError.CONNECT, route + "微服务不可用，请稍后再试..."));
                 return new ByteArrayInputStream(content.getBytes(StandardCharsets.UTF_8));
             }
 
