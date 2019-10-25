d1afd02b26089afa6a1b8bb295f30117e698faab
麻智敏 <ZhiMinMa@sf-express.com>
2019-10-25 20:10:28 +0800

    调整端口

--- src/main/java/com/ch/cloud/gateway/filter/JwtAuthenticationTokenFilter.java
+++ src/main/java/com/ch/cloud/gateway/filter/JwtAuthenticationTokenFilter.java
@@ -1,6 +1,7 @@
 package com.ch.cloud.gateway.filter;
 
 import com.ch.Constants;
+import com.ch.cloud.client.dto.PermissionDto;
 import com.ch.cloud.client.dto.UserDto;
 import com.ch.cloud.gateway.cli.SsoClientService;
 import com.ch.cloud.gateway.cli.UpmsClientService;
@@ -13,6 +14,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -20,6 +22,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
 
@@ -40,15 +43,16 @@ public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
             // 根据token 获取用户名
             if (!res.isEmpty() && SecurityContextHolder.getContext().getAuthentication() == null) {
                 String username = res.get();
+                List<PermissionDto> permissions = Lists.newArrayList();
+                boolean ok = false;
+                for (PermissionDto dto : permissions) {
+                    AntPathRequestMatcher requestMatcher = new AntPathRequestMatcher(dto.getUrl(), dto.getCode());
+                    ok = requestMatcher.matches(request);
+                }
+                if (!ok) {
+                    //
+                }
 
-                Result<UserDto> res1 = upmsClientService.findUserByUsername(username);
-                // 通过用户名 获取用户的信息
-                UserDetails userDetails = new User(res1.get().getUsername(),res1.get().getPassword(), Lists.newArrayList());
-                // 然后把构造UsernamePasswordAuthenticationToken对象
-                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-                // 最后绑定到当前request中，在后面的请求中就可以获取用户信息
-                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                SecurityContextHolder.getContext().setAuthentication(authentication);
             }
         }
         chain.doFilter(request, response);
