16520f0658bd380bcc390914739aef5646aded8f
mazhimin <mazhimin@12301.cc>
2021-09-03 16:41:26 +0800

    升级Jar包

--- src/main/java/com/ch/cloud/gateway/pojo/CacheType.java
+++ src/main/java/com/ch/cloud/gateway/pojo/CacheType.java
@@ -1,7 +1,7 @@
 package com.ch.cloud.gateway.pojo;
 
 import com.ch.Constants;
-import com.ch.utils.StringExtUtils;
+import com.ch.utils.StringUtilsV2;
 
 /**
  * desc:
@@ -24,6 +24,6 @@ public enum CacheType {
         if (args == null) {
             return key;
         }
-        return key + Constants.SECURITY_SEPARATOR + StringExtUtils.linkStr(Constants.SECURITY_SEPARATOR, args);
+        return key + Constants.SECURITY_SEPARATOR + StringUtilsV2.linkStr(Constants.SECURITY_SEPARATOR, args);
     }
 }
